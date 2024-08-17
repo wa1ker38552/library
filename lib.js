37ec2160da3697010128fe08cbf6dcc12b1f6e54
Caden <100868154+wa1ker38552@users.noreply.github.com>
2024-08-16 17:23:08 -0700

    Update lib.js

--- lib.js
+++ lib.js
@@ -13,3 +13,20 @@ async function request(url) {
   const _ = await fetch(url)
   return await _.json()
 }
+
+function animateOpenModal(m) {
+  m.style.display = ""
+  m.style.opacity = "1"
+  m.style.background = "rgba(0, 0, 0, 0.8)"
+  m.style.animation = "fade-in 0.3s"
+  m.children[0].style.animation = "move-up 0.3s"
+}
+
+function animateCloseModal(m) {
+  setTimeout(function() {
+    m.style.display = "none"
+  }, 301)
+  m.children[0].style.animation = "move-down 0.3s"
+  m.style.animation = "fade-out 0.3s"
+  m.style.opacity = 0
+}
